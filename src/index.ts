7792433e9903906928e2e432b0e72b994d333a08
Amebus <federico.ceruti@gmail.com>
2022-12-04 15:43:23 +0100

    #13 removed module augmentation to inject functions into linqIterable prototype

--- src/index.ts
+++ src/index.ts
@@ -1,80 +1,11 @@
-import { LinqIterable } from "./linqIterable";
-
-import {
-  Comparer,
-  Mapper,
-  Predicate
-} from '@/types';
-
-import { 
-  filter
-} from "@/functions";
-
-import {
-  map,
-  materialize,
-  max,
-  memoize,
-  min
-} from '@/functions';
+import { LinqIterable, type ILinqIterable } from "./linqIterable";
 
 import { 
   range
 } from "@/functions";
 
-import {
-  some
-} from "@/functions";
-
-export interface ILinqIterable<T> {
-
-  [Symbol.iterator](): Iterator<T, any, undefined>;
-
-  collectToArray(): T[];
-
-  filter( predicate: Predicate<T>): ILinqIterable<T>; 
-
-  map<R>(mapper: Mapper<T, R>): ILinqIterable<R>;
-  materialize(): ILinqIterable<T>;
-  // max<T>(comparer?: Comparer<T>): T | null | undefined;
-  memoize(): ILinqIterable<T>;
-  // min<T>(comparer?: (a: T, b: T, index: number) => boolean): T | null | undefined;
-
-  some(predicate: (value: T, index: number) => boolean): boolean;
-}
-
-declare module "./linqIterable" {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  interface LinqIterable<T> extends ILinqIterable<T> {
-    
-  }
-}
 
 export function from<T>(iterable: Iterable<T>): ILinqIterable<T> {
-	return new LinqIterable(iterable);
+  return new LinqIterable(iterable);
 }
-
-LinqIterable.prototype.filter = function<T>(predicate: (value: T, index: number) => boolean): ILinqIterable<T> {
-  return from(filter(this.source, predicate));
-};
-
-
-LinqIterable.prototype.map = function<T,R>(mapper: (value: T, index: number) => R): ILinqIterable<R> {
-  return from(map(this.source, mapper));
-};
-LinqIterable.prototype.materialize = function<T>(): ILinqIterable<T> {
-  return from(materialize(this.source));
-};
-// LinqIterable.prototype.max = function<T>(comparer?: (a: T, b: T, index: number) => boolean): T | null | undefined {
-//   return max(this.source(), comparer);
-// };
-LinqIterable.prototype.memoize = function<T>(): ILinqIterable<T> {
-  return from(memoize(this.source));
-};
-// LinqIterable.prototype.min = function<T>(comparer?: (a: T, b: T, index: number) => boolean): T | null | undefined {
-//   return min(this.source(), comparer);
-// };
-
-LinqIterable.prototype.some = function<T>(predicate: (value: T, index: number) => boolean): boolean {
-  return some(this.source, predicate);
-};
+export { ILinqIterable };
--- src/linqIterable.ts
+++ src/linqIterable.ts
@@ -1,4 +1,45 @@
-export class LinqIterable<T> implements Iterable<T> {
+import {
+  Comparer,
+  Mapper,
+  Predicate
+} from '@/types';
+
+import { 
+  filter
+} from "@/functions";
+
+import {
+  map,
+  materialize,
+  max,
+  memoize,
+  min
+} from '@/functions';
+
+import {
+  some
+} from "@/functions";
+
+
+
+export interface ILinqIterable<T> {
+
+  [Symbol.iterator](): Iterator<T, any, undefined>;
+
+  collectToArray(): T[];
+
+  filter(predicate: Predicate<T>): ILinqIterable<T>; 
+
+  map<R>(mapper: Mapper<T, R>): ILinqIterable<R>;
+  materialize(): ILinqIterable<T>;
+  // max<T>(comparer?: Comparer<T>): T | null | undefined;
+  memoize(): ILinqIterable<T>;
+  // min<T>(comparer?: (a: T, b: T, index: number) => boolean): T | null | undefined;
+
+  some(predicate:  Predicate<T>): boolean;
+}
+
+export class LinqIterable<T> implements ILinqIterable<T> {
 
 	constructor(iterable: Iterable<T>) {
 		this.iterable = iterable;
@@ -10,10 +51,32 @@ export class LinqIterable<T> implements Iterable<T> {
 	private iterable: Iterable<T>;
 
 	collectToArray(): T[] {
-		return [...this.source];
+		return [...this.iterable];
 	}
 
-	get source(): Iterable<T> {
-		return this.iterable;
+	filter(predicate: Predicate<T>): ILinqIterable<T> {
+		return new LinqIterable(filter(this.iterable, predicate));
+	}
+
+	map<R>(mapper: Mapper<T, R>): ILinqIterable<R> {
+		return new LinqIterable(map(this.iterable, mapper));
+	}
+	materialize(): ILinqIterable<T> {
+		return new LinqIterable(materialize(this.iterable));
+	}
+	memoize(): ILinqIterable<T> {
+		return new LinqIterable(memoize(this.iterable));
+	}
+
+	some(predicate: Predicate<T>): boolean {
+		return some(this.iterable, predicate);
 	}
 }
+
+// LinqIterable.prototype.max = function<T>(comparer?: (a: T, b: T, index: number) => boolean): T | null | undefined {
+//   return max(this.source(), comparer);
+// };
+
+// LinqIterable.prototype.min = function<T>(comparer?: (a: T, b: T, index: number) => boolean): T | null | undefined {
+//   return min(this.source(), comparer);
+// };
