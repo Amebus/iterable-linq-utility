a67bca776f28056fd68ec2624f4cc4cfea10fe31
Amebus <federico.ceruti@gmail.com>
2022-12-13 16:45:04 +0100

    #8 fromRange

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,13 @@ export function from<T>(iterable: Iterable<T>): ILinqIterable<T> {
   return new LinqIterableWrapper(iterable);
 }
 
-export function fromRange(end: number): Iterable<number>;
-export function fromRange(end: number, reverse?: boolean): Iterable<number>;
-export function fromRange(start: number, end: number): Iterable<number>;
-export function fromRange(start: number, end: number, step: number): Iterable<number>;
-export function fromRange(start: number, end: number, reverse: boolean): Iterable<number>;
-export function fromRange(start: number, end: number, step: number, reverse: boolean): Iterable<number>;
-export function fromRange(start: number, end?: number | boolean, step?: number | boolean, reverse?: boolean): Iterable<number> {
+export function fromRange(end: number): ILinqIterable<number>;
+export function fromRange(end: number, reverse?: boolean): ILinqIterable<number>;
+export function fromRange(start: number, end: number): ILinqIterable<number>;
+export function fromRange(start: number, end: number, step: number): ILinqIterable<number>;
+export function fromRange(start: number, end: number, reverse: boolean): ILinqIterable<number>;
+export function fromRange(start: number, end: number, step: number, reverse: boolean): ILinqIterable<number>;
+export function fromRange(start: number, end?: number | boolean, step?: number | boolean, reverse?: boolean): ILinqIterable<number> {
   return from(range(start, end, step, reverse));
 }
 
